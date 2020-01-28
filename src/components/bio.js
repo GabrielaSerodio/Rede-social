5d0a233d8d1a9b90b21dd028b21a452c7b2ab85d
Laboratória <gabrielav22@outlook.com>
2020-01-28 09:40:01 -0300

    atualizando gif

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,13 +1,13 @@
 const Bio = (user) => {
   const template = `
-  <div class="users">
-  <img src="${user.image || 'https://cdn1.iconfinder.com/data/icons/navigation-elements/512/user-login-man-human-body-mobile-person-512.png'}" class="img-usuario">
-    <div class="bio">
-    <p><strong>Nome:</strong> ${user.name || 'Nome'}</p>
-    <p><strong>Idade:</strong> ${user.age || 'Idade'} anos</p>
-    <p><strong>Profissão:</strong> ${user.profession || 'Profissão'}</p>
-    </div>
-  </div>
+    <section class="image-user">
+      <img src="${user.image || 'https://cdn1.iconfinder.com/data/icons/navigation-elements/512/user-login-man-human-body-mobile-person-512.png'}" class="img-usuario">
+      <section class="bio">
+      <p><strong>Nome:</strong> ${user.name || 'Nome'}</p>
+      <p><strong>Idade:</strong> ${user.age || 'Idade'} anos</p>
+      <p><strong>Profissão:</strong> ${user.profession || 'Profissão'}</p>
+      </section>
+    </section>
   `;
   return template;
 };
